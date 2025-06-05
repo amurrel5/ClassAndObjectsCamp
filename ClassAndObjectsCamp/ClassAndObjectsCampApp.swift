4fe128809798af22ea05c2c3221ba29d0aa7eed0
Ashley <anm38060@gmail.com>
2025-06-05 12:56:31 -0500

    Initial Commit

---
+++ ClassAndObjectsCamp/ClassAndObjectsCampApp.swift
@@ -0,0 +1,17 @@
+//
+//  ClassAndObjectsCampApp.swift
+//  ClassAndObjectsCamp
+//
+//  Created by Scholar on 6/5/25.
+//
+
+import SwiftUI
+
+@main
+struct ClassAndObjectsCampApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
