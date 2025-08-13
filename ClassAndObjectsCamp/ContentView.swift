80e7f4f88994f81db8d645e72d22ee8cd74e3c54
Ashley <anm38060@gmail.com>
2025-08-12 22:00:02 -0500

    classes and objects code

--- ClassAndObjectsCamp/ContentView.swift
+++ ClassAndObjectsCamp/ContentView.swift
@@ -4,19 +4,31 @@
 //
 //  Created by Scholar on 6/5/25.
 //
-
+//Classes and objects playground
 import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        class Scholar {
+            var grade = 10
+            let studying = "Swift"
+            var name = "Ophelia"
+            init (scholarName: String, scholarGrade: Int) {  //makes sure I can change the name
+                name = scholarName
+                grade = scholarGrade
+            }
+            func writeCode() {
+                print("\(name) is busy writing code!")
+            }
         }
-        .padding()
-    }
+        var newScholar = Scholar(scholarName: "Avanti", scholarGrade: 15)
+        var newScholar2 = Scholar(scholarName: "Sarah", scholarGrade: 7)
+        print(newScholar.name)
+        print(newScholar2.grade)
+
+        newScholar.writeCode()
+        newScholar2.writeCode()
+  }
 }
 
 #Preview {
